1d5c5aab21598db9d2fcaadf629d5b43a4105489
Сергей <7atom7@mail.ru>
2022-01-17 17:44:29 +0300

    Код обернут в функцию, исправлен README и requirements, переименованы переменные

--- Lesson-1 Weather from terminal.py
+++ Lesson-1 Weather from terminal.py
@@ -1,17 +1,17 @@
-from cgitb import text
 import requests
 
-'''
-Для всех трех сразу
-'''
-RAW = ['london','svo','cherepovets']
-for i in RAW:
-  url_template = 'https://wttr.in/{}'
-  url = url_template.format(i)
-  parameters = {
-    'lang': 'ru',
-    '?': 'm'
-  }
-  response = requests.get(url, parameters)
-  response.raise_for_status()
-  print('\n\n', response.text, '\n')
+
+def weather_in_town(places):
+  for i in places:
+    url_template = f'https://wttr.in/{i}'
+    request_parameters = {
+      'lang': 'ru',
+      'F': 'm'
+    }
+    response = requests.get(url_template, request_parameters)
+    response.raise_for_status()
+    print('\n\n', response.text, '\n')
+
+if __name__ == '__main__':
+  places = ['london','svo','cherepovets']
+  weather_in_town(places)
